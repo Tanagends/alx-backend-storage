6f906a2aa42ef78d919de70583ac2d9abca37b4e
Tanagends <tanagends@gmail.com>
2024-05-09 23:47:44 +0200

    sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,4 @@
+-- Write a SQL script that creates a view need_meeting that lists all students
+CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
+
